a3ac7bbc353d4fed5e8db3cee1be60a0ffba8ea7
Sergii Zaitsev <sergii.zaitsev@sixt.com>
2020-03-02 14:57:23 +0100

    use platform-go embedded go-micro

--- plugin/micro/micro.go
+++ plugin/micro/micro.go
@@ -14,8 +14,8 @@ import (
 // relative to the import_prefix of the generator.Generator.
 const (
 	contextPkgPath = "context"
-	clientPkgPath  = "github.com/micro/go-micro/client"
-	serverPkgPath  = "github.com/micro/go-micro/server"
+	clientPkgPath  = "github.com/sixt/com.sixt.lib.platform-go/x/micro/go-micro/client"
+	serverPkgPath  = "github.com/sixt/com.sixt.lib.platform-go/x/micro/go-micro/server"
 )
 
 func init() {
